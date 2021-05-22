5633ccd256802279055cee2ae83c07d038a7f42e
Rudi Häusler <commerce@haeuslernet.de>
2021-05-22 15:05:04 +0200

    Version 1.0.1
    - added QR Code Settings
    - Plugin Class Name changed

--- README.md
+++ README.md
@@ -21,15 +21,46 @@ From Obsidian v0.9.8, you can activate this plugin within Obsidian by doing the 
 
 ## Usage
 
-Type the `qrcode` keyword to use the QR code plugin.
+Type the `qrcode` keyword to use the QR Code Plugin.
 
-![Screenshot](https://raw.githubusercontent.com/rudimuc/obsidian-qrcode/main/obsidian_raw.png)
+```yaml
+    ```qrcode
+        https://github.com
+    ```
+```
 
 The result will be this:
 
 ![Screenshot](https://raw.githubusercontent.com/rudimuc/obsidian-qrcode/main/obsidian_rendered.png)
 
+For complex QR Code generation (which means customizable codes) use the keyword `qrcode-complex` instead.
+
+```yaml
+    ```qrcode-complex
+    {
+        "text": "this is my data",
+        "width": 400,
+        "margin": 20,
+        "dark": "#0FF",
+        "light": "#FFF"
+    }
+    ```
+```
+
+You have the following options:
+
+|parameter|required|example|
+|--|--|--|
+|text  |yes  | The data/content for the code
+|width|optional (default = auto)| integer value for the size
+|margin|optional (default = 4)| Define how much wide the quiet zone should be.
+|dark|optional (default = #000000)| RGB or RGBA Hex-Code for the dark Pixels
+|light|optional (default = #FFFFFF)| RGB or RGBA Hex-Code for the light Pixels
 
 ## Version History
+### 1.0.1
+- New Keyword for complex QR Codes with settings
+- Plugin class name changed
 ### 1.0.0
 - Initial Release
+
--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import { Plugin, MarkdownPostProcessorContext } from 'obsidian';
-import QRCode from 'qrcode';
+import { Plugin, MarkdownPostProcessorContext, Notice } from 'obsidian';
+import QRCode from 'qrcode'
 
-export default class MyPlugin extends Plugin {
+export default class QrCodePlugin extends Plugin {
 	
-	postprocessor = async (content: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+	postprocessorRaw = async (content: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
 		const destination = document.createElement('canvas');
 		if(content.endsWith("\n")) {
 			// Obsidian gives an unpretty linebreak at the end. Don't encode it in our QR Code!
@@ -14,9 +14,37 @@ export default class MyPlugin extends Plugin {
 		return;
 	}
 
+	postprocessorComplex = async (content: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+		const destination = document.createElement('canvas');
+		let [text, parameters] = this.readParameters(content);
+		QRCode.toCanvas(destination, text, parameters);
+		el.appendChild(destination);
+		return;
+	}
+	
+	private readParameters(jsonString: any) {
+		let params = JSON.parse(jsonString);
+		var options: {[k:string]: any} = {};
+		options.color = {light: "#ffffff", dark: "#000000"};
+		if (params.width !== undefined) {
+			options.width = params.width;
+		}
+		if (params.margin !== undefined) {
+			options.margin = params.margin;
+		}
+		if (params.dark !== undefined) {
+			options.color.dark = params.dark;
+		}
+		if (params.light !== undefined) {
+			options.color.light = params.light;
+		}
+		return [params.text, options];
+	}
+
 	async onload() {
 		console.log('loading plugin');
-		this.registerMarkdownCodeBlockProcessor('qrcode', this.postprocessor);
+		this.registerMarkdownCodeBlockProcessor('qrcode', this.postprocessorRaw);
+		this.registerMarkdownCodeBlockProcessor('qrcode-complex', this.postprocessorComplex);
 	}
 
 	onunload() {
